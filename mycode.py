a8429a28a2e5dd305580ebe0dd849687c53bd56b
G736saini <sainigajanand003@gmail.com>
2025-03-21 16:44:58 +0530

    Add files via upload

--- mycode.py
+++ mycode.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 import pandas as pd
 import os
 
@@ -27,4 +28,35 @@ file_path = os.path.join(data_dir, 'sample_data.csv')
 # Save the DataFrame to a CSV file, including column names
 df.to_csv(file_path, index=False)
 
+=======
+import pandas as pd
+import os
+
+# Create a sample DataFrame with column names
+data = {'Name': ['Alice', 'Bob', 'Charlie'],
+    'Age': [25, 30, 35],
+    'City': ['New York', 'Los Angeles', 'Chicago']
+    }
+
+df = pd.DataFrame(data)
+
+# # Adding new row to df for V2
+# new_row_loc = {'Name': 'GF1', 'Age': 20, 'City': 'City1'}
+# df.loc[len(df.index)] = new_row_loc
+
+# # Adding new row to df for V3
+# new_row_loc2 = {'Name': 'GF2', 'Age': 30, 'City': 'City2'}
+# df.loc[len(df.index)] = new_row_loc2
+
+# Ensure the "data" directory exists at the root level
+data_dir = 'data'
+os.makedirs(data_dir, exist_ok=True)
+
+# Define the file path
+file_path = os.path.join(data_dir, 'sample_data.csv')
+
+# Save the DataFrame to a CSV file, including column names
+df.to_csv(file_path, index=False)
+
+>>>>>>> bc22917e46eff28e284457abc4ba9e0950f16461
 print(f"CSV file saved to {file_path}")
